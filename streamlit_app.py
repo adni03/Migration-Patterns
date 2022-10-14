0f5cba8d2e7a5c54e84677f4fe5c7e8f060d61d8
adni03 <srisai@student.nitw.ac.in>
2022-10-13 22:00:35 -0400

    emoji stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,9 +173,8 @@ distance_moved_race_q_bar = alt.Chart(
 
 avg_distance = global_average_distance(base_df, lat_lon_df)
 
-# miles_moved_race_df['icon'] = ['👨', '👨', '👨', '👨', '👨']
-miles_moved_race_df['icon'] = ['a', 'a', 'a', 'a', 'a']
-avg_df = pd.DataFrame({'Name': 'National Average', 'Value': avg_distance, 'icon': 'a'}, index=[0])
+miles_moved_race_df['icon'] = ["\N{man}", "\N{man}", "\N{man}", "\N{man}", "\N{man}"]
+avg_df = pd.DataFrame({'Name': 'National Average', 'Value': avg_distance, 'icon': "\N{house}"}, index=[0])
 
 race_dist = alt.Chart(
     miles_moved_race_df,
